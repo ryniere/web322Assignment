07d42c2ff83b7008529a1bdb26d003135bcbce59
Ryniere Silva <ryniere@espresslabs.com>
2020-02-24 21:21:27 -0500

    adding por env

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post("/submitRequestRydeForm",(req,res)=>{
 });
 
 
-const PORT=3000;
+const PORT =process.env.PORT || 3000;
 //This creates an Express Web Server that listens to HTTP Reuqest on port 3000
 app.listen(PORT,()=>{
     console.log(`Web Server Started`);
